56149986a499ed2b566b93a6645cfd3702b0ceef
UNKNOWN <rajr86642@gmail.com>
2025-05-18 01:17:48 +0530

    Initial Commit

---
+++ BackEnd/MediServe/src/main/java/com/roy/medi_serve/com/MediServe/MediServeApplication.java
@@ -0,0 +1,13 @@
+package com.roy.medi_serve.com.MediServe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediServeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediServeApplication.class, args);
+	}
+
+}
---
+++ BackEnd/MediServe/src/test/java/com/roy/medi_serve/com/MediServe/MediServeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roy.medi_serve.com.MediServe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediServeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
